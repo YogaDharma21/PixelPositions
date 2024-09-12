07d2dcc6a23aefbc7c55ad48f1b815ab6217a80e
YogaDharma21 <yogacode2006@gmail.com>
2024-09-12 21:20:22 +0800

    fix: update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,10 @@ export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
   theme: {
-    extend: {
-      colors: {
-        'black': '#060606',
-      },
-      fontFamily:{
-        "hanken-grotesk":["Hanken Grotesk",'sans-serif']
-      },
-      fontSize:{
-        "2xs":"0.625rem", // 10px
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
