fc48d2590ad4ed58d36585f2d1a4e737a47bb45b
YogaDharma21 <yogacode2006@gmail.com>
2024-09-21 11:43:19 +0800

    feat: auth system finished

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,7 +16,7 @@
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
-                <a href="">
+                <a href="/">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
@@ -26,9 +26,22 @@
                 <a href="#">Salaries</a>
                 <a href="#">Companies</a>
             </div>
-            <div>
-                <a href="">Post a job</a>
+            @auth
+            <div class="space-x-6 font-bold flex ">  
+                <a href="/jobs/create">Post a job</a>
+                <form action="/logout" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button>Log out</button>
+                </form>
             </div>
+            @endauth
+            @guest
+            <div class="space-x-6 font-bold">
+                <a href="/register">Sign Up</a>
+                <a href="/login">Log In</a>
+            </div>
+            @endguest
         </nav>
         <main class="mt-10 max-w-[986px] mx-auto">
             {{ $slot }}
